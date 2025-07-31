2a0110ba9b51aea1724ff068c8d6ab479e4e31d8
Ashwind P <ashwindpanchalingam@gmail.com>
2025-07-31 09:57:47 +0530

    Add files via upload

---
+++ lenstr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello";
+    int len = strlen(str);
+
+    printf("Length of string \"%s\" is: %d\n", str, len);
+
+    return 0;
+}
+
